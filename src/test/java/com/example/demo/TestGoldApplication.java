9583a144403fb021e5d96daec56e82c59cdd68fb
Tim Sparg <6872586+timothysparg@users.noreply.github.com>
2026-02-13 21:40:35 +0100

    initial commit

---
+++ src/test/java/com/example/demo/TestGoldApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestGoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(GoldApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
