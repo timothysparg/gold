4bb644e26a4f8592e472d8867f87c5e86cfb4c0d
Tim Sparg <6872586+timothysparg@users.noreply.github.com>
2026-02-14 01:19:41 +0100

    Add google-java-format linter

--- src/main/java/com/example/demo/GoldApplication.java
+++ src/main/java/com/example/demo/GoldApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GoldApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GoldApplication.class, args);
+  }
 }
--- src/test/java/com/example/demo/GoldApplicationTests.java
+++ src/test/java/com/example/demo/GoldApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class GoldApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
